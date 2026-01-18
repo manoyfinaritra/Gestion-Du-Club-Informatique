abb3979a5a114116375f2aebf06f30e6681ba6d1
manoyfinaritra <finaritra@MACdeMacBook-Pro-2.local>
2026-01-19 01:22:12 +0300

    modification database

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Structure de la table `membres`
+-- Hôte : localhost
+-- Généré le : dim. 18 jan. 2026 à 23:18
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
-CREATE TABLE `membres` (
+-- Base de données : `club_informatique`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `AddMembres`
+--
+
+CREATE TABLE `AddMembres` (
+  `id` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `age` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `nom_facebook` text NOT NULL,
+  `genre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `nom` text NOT NULL,
   `prenom` text NOT NULL,
@@ -11,15 +53,39 @@ CREATE TABLE `membres` (
   `mot_de_passe` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Index pour les tables déchargées
+--
 
 --
--- Index pour la table `membres`
+-- Index pour la table `AddMembres`
 --
-ALTER TABLE `membres`
+ALTER TABLE `AddMembres`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour la table `membres`
+-- Index pour la table `users`
 --
-ALTER TABLE `membres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `AddMembres`
+--
+ALTER TABLE `AddMembres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

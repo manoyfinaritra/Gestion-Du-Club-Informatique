42f383a409c4f1ab6353e9bf43561809dac656d9
Lovanirina R <lovanirina.ran@gmail.com>
2026-01-18 16:06:48 +0300

    feat: add basic services structure

---
+++ assets/js/services.js
@@ -0,0 +1,32 @@
+/**
+ * Get all members
+ */
+const getAllMembers = () => {
+  // TODO: Implement
+};
+
+/**
+ * Get one member by Id
+ *
+ * @param id is a number
+ */
+const getMemberById = (id) => {
+  // TODO: Implement
+};
+
+/**
+ * Update member using his Id
+ *
+ * @param id is a number
+ * @param data is an object that match the member schema
+ */
+const updateMemberById = (id, data) => {
+  // TODO: Implement
+};
+
+/**
+ * Delete a member matching the given id
+ */
+const deleteMemberById = (id) => {
+  // TODO: Implement
+};

8dd3a9148ce4788b5cbb4934c5c721e7c614559a
Lovanirina R <lovanirina.ran@gmail.com>
2026-01-19 19:00:11 +0300

    fix: css order

--- connexion.php
+++ connexion.php
@@ -7,9 +7,9 @@
     <title>Se connecter</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.css" />
     <link rel="stylesheet" href="./assets/css/icon.css" />
-    <link rel="stylesheet" href="assets/css/auth.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="assets/css/auth.css" />
 </head>
 
 <body>
